20badc7ab5ce1bbabb19a52c306a858a8269a6f4
jiang_jianmin <402766510@qq.com>
2022-09-08 14:59:15 +0800

    add cast

---
+++ C++/project_cast/project_cast/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a = 10;
+
+	// 8.25 -> 1000.01 -> 1.x*2^y(3指数，00001尾数)
+	double d = reinterpret_cast<double&>(a);
+
+	cout << "a = " << a << endl;
+	cout << "d = " << d << endl;
+	return 0;
+}
