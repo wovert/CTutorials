dbfa0f9cb5d5eebb587ed25cd5e8406078dea0a9
wovert <wovert@126.com>
2022-09-16 17:11:05 +0800

    add windows

---
+++ windows/project_files/project_files/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	bool res;
+	//char path[] = "C:\\Users\\Administrator\\Desktop\\abc.txt";
+	char path[] = "C:/Users/Administrator/Desktop/abc.txt";
+
+	// delete file
+	res = DeleteFileA(path);
+	cout << "res=" << res << endl;
+	// https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499-
+	cout << "GetLastError=" << GetLastError() << endl;
+	getchar();
+	return 0;
+}
