9517f24878c407e1c580308f66dce950d3db178c
wovert <wovert@126.com>
2022-09-29 10:29:52 +0800

    add mmap

---
+++ C/IPC/mmap/mmap.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main() {
+  int fd = -1;
+  int res = -1;
+  void *addr = NULL;
+
+  // 读写方式打开一个文件
+  fd = open("./txt", O_RDWR);
+  if (-1 == fd) {
+    perror("open");
+    return 1;
+  }
+
+  // 将文件映射到内存
+  addr = mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (addr == MAP_FAILED) {
+    perror("mmap");
+    return 1;
+  }
+
+  printf("文件存储映射成功\n");
+
+  close(fd);
+
+  // 写文件
+  memcpy(addr, "1234567890", 10);
+
+  // 断开存储映射
+  munmap(addr, 1024);
+
+  return 0;
+}
---
+++ C/IPC/mmap/mmap_fork.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+
+
+int main() {
+  pid_t pid = -1;
+  int fd = -1;
+  int res = -1;
+  void *addr = NULL;
+
+  // 读写方式打开一个文件
+  fd = open("./txt", O_RDWR);
+  if (-1 == fd) {
+    perror("open");
+    return 1;
+  }
+
+  // 将文件映射到内存
+  addr = mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (addr == MAP_FAILED) {
+    perror("mmap");
+    return 1;
+  }
+
+  printf("文件存储映射成功\n");
+
+  close(fd);
+
+  pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    return 1;
+  }
+
+  // child process
+  if (0 == pid) {
+    // 写文件
+    memcpy(addr, "ABCDEFGHIJ", 10);
+
+  } else {
+   // parent process
+   // 等待子进程结束
+   wait(NULL);
+   printf("父进程读取 add=%s\n", (char *)addr);
+  }
+
+
+  // 断开存储映射
+  munmap(addr, 1024);
+
+  return 0;
+}
