800bb72c7c24f3492a7fa56a503bf200ed716542
wovert <wovert@126.com>
2023-02-09 10:22:46 +0900

    chnage

---
+++ C/06-array/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define ARRAY_SIZE 10
+
+// 默认都是0
+int global_array[ARRAY_SIZE];
+
+int main() {
+
+  // 定义数组
+  auto int array[ARRAY_SIZE];
+  int array0[ 3 + 2]; // ok
+
+  for (int i = 0; i < ARRAY_SIZE; ++i) {
+    printf("array[%d]=%c\n", i, array[i]);
+  }
+
+  for (int i = 0; i < ARRAY_SIZE; ++i) {
+    array[i] = i;
+  }
+
+  for (int i = 0; i < ARRAY_SIZE; ++i) {
+    printf("array[%d]=%c\n", i, array[i]);
+  }
+
+  // 初始化列表
+  int array2[ARRAY_SIZE] = {0,1,2,3,4,5,6,7,8,9};
+
+  for (int i = 0; i < ARRAY_SIZE; ++i) {
+    printf("array2[%d]=%c\n", i, array[i]);
+  }
+
+  // C99
+  char array_char[5] = {[2] = '0', 'l', 'k'};
+  for (int i = 0; i < 5; ++i) {
+    printf("array_char[%d]=%c\n", i, array_char[i]);
+  }
+
+  int value = 2;
+
+  // C99, VLA: gcc 支持； MSVC ERROR
+  int array_size_of_value[value];
+
+  const int kSize = 5; // C++ ok
+  int array_size_of_const[kSize]; // C99, VLA: gcc 支持； MSVC ERROR
+
+
+  return 0;
+}
