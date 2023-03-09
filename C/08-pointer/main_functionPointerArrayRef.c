9014a9019b9b5ce045e3ac09c42517dacc543749
wovert <wovert@126.com>
2023-03-09 15:57:17 +0900

    chnage

--- C/08-pointer/main_functionPointerArrayRef.c
+++ C/08-pointer/main_functionPointerArrayRef.c
@@ -12,18 +12,18 @@ int rich(int *);
 int middle(int *);
 int poor(int *);
 
-
+RATE_FUNC *funcArr[3] = {NULL};
 int prices[3] = {10000, 10000, 10000};
 
-//int (*funcArr[3])(int *);
-
 RATE_FUNC *callFunc(int *a) {
-
   RATE_FUNC *item1 = rich;
   RATE_FUNC *item2 = middle;
   RATE_FUNC *item3 = poor;
 //  int (*funcArr[3])(int *) = {item1, item2, item3};
-  RATE_FUNC *funcArr[3] = {item1, item2, item3};
+//  RATE_FUNC *funcArr[3] = {item1, item2, item3};
+  funcArr[0] = item1;
+  funcArr[1] = item2;
+  funcArr[2] = item3;
 
   return funcArr;
 }
@@ -47,21 +47,15 @@ int main() {
     printf("prices[%d]=%d\n", j, prices[j]);
   }
 
-
   int (*(*(*pRate)(int *))[3])(int *) = callFunc;
-//  printf("pRate=%p\n", pRate);
 
 
   RATE_FUNC **arr = *pRate(&prices[0]);
-//  (*arr[0])(&prices[0]);
-//  (*arr[1])(&prices[1]);
-//  (*arr[2])(&prices[2]);
+
 
   for (int i = 0; i < 3; ++i) {
 //    (*(arr[i]))(&prices[i]);
     (*(arr + i))(&prices[i]);
-//    printf("pRate[%d]=%p\n", i, ((pRate)(&prices[i]))[i](&prices[i]) );
-
   }
   for(int j=0;j<3; j++) {
     printf("prices[%d]=%d\n", j, prices[j]);
