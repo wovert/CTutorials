33b9cc400dd2802722be7cd15b608ae574647014
wovert <wovert@126.com>
2023-03-21 19:48:57 +0900

    change

--- C++/IO类库/IO类库/main.cpp
+++ C++/IO类库/IO类库/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <string>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -118,8 +120,171 @@ void test08() {
 		cout << cin.fail() << endl;
 	}
 }
+
+// 标准输出流
+void test09() {
+	cout.put('c');
+
+	// 链式编程
+	cout.put('a').put('a').put('\n');
+
+	// 输出
+	char buf[] = "hello world";
+	cout.write(buf, strlen(buf));
+	cout.put('\n');
+
+	// 通过流函数实现格式化
+	int num = 99;
+	cout.width(20); // 设置宽度
+	cout.fill('*'); // 填充
+	cout.setf(ios::left); // 数据在左边
+	cout.unsetf(ios::dec); // 卸载十进制
+	cout.setf(ios::hex); // 设置16进制
+	cout.setf(ios::showbase); // 显示基数
+	cout.unsetf(ios::hex);
+	cout.setf(ios::oct); // 显示8进制
+	cout << num << endl;
+
+
+}
+void test10() {
+	// 空过控制符格式化输出，引入头文件iomanp
+	int no = 255;
+	cout << setw(20); // 设置宽度
+	cout << setfill('~');
+	cout << setiosflags(ios::showbase);
+	cout << setiosflags(ios::left);
+	cout << hex;
+	cout << oct;
+	cout << dec;
+	cout << no << endl;
+}
+void test11() {
+	double d = 22.22;
+	cout << setiosflags(ios::fixed); // 设置浮点数
+	cout << setprecision(10); // 小数点后留多少位
+	cout << d << endl;
+}
+void test12() {
+	// 信息输出到缓冲区，然后写到文件
+	ofstream ofs;
+	
+
+	// 打开文件，以写的方式，没有文件，则创建文件
+	ofs.open("test.txt", ios::out | ios::trunc);
+	if (!ofs.is_open()) {
+		cout << "打开失败" << endl;
+	}
+	// 写文件
+	ofs << "姓名：悟空" << endl;
+	ofs << "age：20" << endl;
+	ofs << "height：180cm" << endl;
+
+
+	// 关闭文件
+	ofs.close();
+}
+void test13() {
+	// 从文件读取信息输出到缓冲区，然后写到屏幕
+	ifstream ifs;
+
+	// 打开文件，以写的方式，没有文件，则创建文件
+	ifs.open("test.txt", ios::in);
+	if (ifs.is_open() == false) {
+		cout << "打开失败" << endl;
+	}
+	// 第一种方式读取文件
+	// 一行一行读取
+	//char buf[1024] = { 0 };
+	//while (ifs >> buf) {
+	//	cout << buf << endl;
+	//}
+
+	// 第二种方式读文件
+	// 是否到文件末尾
+	//char buf[1024] = { 0 };
+	//while (!ifs.eof()) {
+	//	ifs.getline(buf, sizeof(buf));
+	//	cout << buf << endl;
+	//}
+
+	// 第三种方式读取文件
+	// 单个字符
+	char c;
+	while ((c = ifs.get()) != EOF) {
+		cout << c;
+	}
+
+
+	// 关闭文件
+	ifs.close();
+}
+
+
+class Maker {
+public:
+	Maker() {}
+	Maker(const char *name, int age) {
+		this->age = age;
+		strcpy_s(this->name, 64, name);
+	}
+public:
+	// string类中有一个成员指针char *, 该指针指向了存储字符串的空间
+	// string类中的数据存储到文件中，在读出来时，不能保证指针有效
+	// string 开辟的空间如果大于16个字节就在堆区，小于就在栈区
+	// string name;
+	char name[64];
+	int age;
+};
+
+void test14() {
+	// 写二进制数据
+	Maker m1("悟空", 29);
+	Maker m2("贝吉塔", 32);
+
+	ofstream ofs;
+	ofs.open("test.txt", ios::out | ios::trunc | ios::binary);
+	if (!ofs.is_open()) {
+		cout << "打开失败" << endl;
+	}
+
+	// write
+	ofs.write((const char *)&m1, sizeof(Maker));
+	ofs.write((const char *)&m2, sizeof(Maker));
+
+	ofs.close();
+	
+}
+void test15() {
+	// 读二进制文件
+	ifstream ifs;
+
+	ifs.open("test.txt", ios::in | ios::binary);
+	if (!ifs.is_open()) {
+		cout << "打开失败" << endl;
+	}
+
+	Maker m1;
+	Maker m2;
+
+	ifs.read((char *)&m1, sizeof(Maker));
+	ifs.read((char *)&m2, sizeof(Maker));
+
+	cout << "name:" << m1.name << " age:" << m1.age << endl;
+	cout << "name:" << m2.name << " age:" << m2.age << endl;
+
+	ifs.close();
+
+}
 int main() {
-	test08();
+	test15();
+	//test14();
+	//test13();
+	//test12();
+	//test11();
+	//test10();
+	//test09();
+	//test08();
 	//test07();
 	//test06();
 	//test05();
---
+++ C++/容器/容器/main.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <vector> // 容器
+#include <algorithm> // 算法
+#include <string>
+
+using namespace std;
+
+template<class T>
+class MyArray {
+public:
+	// 保护原生指针，给原生指针取别名
+	typedef T* iterator;
+	MyArray() {
+		mCapacity = 10;
+		mSize = 10;
+		p = new T[mCapacity];
+		for (int i = 0; i < mCapacity; i++) {
+			p[i] = i + 1;
+		}
+	}
+	// 提供迭代器
+	T* begin() {
+		return p;
+	}
+	// 结束位置的迭代器
+	T* end() {
+		return p + mSize;
+	}
+public:
+	T* p;
+	int mCapacity;
+	int mSize;
+};
+
+// 算法设计
+template<class T>
+void print(T begin, T end) {
+	for (; begin != end; ++begin) {
+		cout << *begin << " ";
+	}
+	cout << endl;
+}
+
+class Maker {
+public:
+	string name;
+	int age;
+public:
+	Maker() {
+
+	}
+	Maker(string name, int age) {
+		this->name = name;
+		this->age = age;
+	}
+};
+
+ostream &operator<<(ostream &out, Maker &m) {
+	out << "name=" << m.name << " age=" << m.age << endl;
+	return out;
+}
+
+void test01() {
+	MyArray<int> arr;
+
+	// 获取容器提供的开始位置的迭代器
+	MyArray<int>::iterator begin = arr.begin();
+	// 获取容器提供的结束位置迭代器
+	MyArray<int>::iterator end = arr.end();
+
+	print(begin, end);
+}
+
+// 加入算法的回调函数
+void MyPrint(int val) {
+	cout << val << " ";
+}
+void test02() {
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
+
+	// 获取开始位置的迭代器
+	vector<int>::iterator begin = v.begin();
+	// 获取结束位置的迭代器
+	vector<int>::iterator end = v.end();
+
+	// 遍历算法
+	for_each(begin, end, MyPrint);
+	cout << endl;
+
+}
+void test03() {
+	vector<Maker> v;
+	v.push_back(Maker("悟空1", 18));
+	v.push_back(Maker("悟空2", 28));
+	v.push_back(Maker("悟空3", 38));
+	v.push_back(Maker("悟空4", 48));
+	v.push_back(Maker("悟空5", 58));
+
+	// 获取开始和结束位置的迭代器
+	vector<Maker>::iterator begin = v.begin();
+	vector<Maker>::iterator end = v.end();
+
+	while (begin != end) {
+		cout << (*begin);
+		begin++;
+	}
+}
+void test04() {
+	// 存储对象的指针
+	vector<Maker *> v;
+	Maker *m1 = new Maker("悟空1", 18);
+	Maker *m2 = new Maker("悟空2", 28);
+	Maker *m3 = new Maker("悟空3", 38);
+	v.push_back(m1);
+	v.push_back(m2);
+	v.push_back(m3);
+
+	// 获得开始指针
+	vector<Maker *>::iterator begin = v.begin();
+	vector<Maker *>::iterator end = v.end();
+
+	while(begin != end) {
+		cout << (*begin)->name << " " << (*begin)->age << endl;
+		++begin;
+	}
+}
+// 容器嵌套容器
+void test05() {
+	vector<vector<int>> vs;
+
+	vector<int> v1;
+	vector<int> v2;
+	vector<int> v3;
+
+	for(int i=0; i<3; i++){
+		v1.push_back(i + 10);
+		v2.push_back(i + 10);
+		v3.push_back(i + 10);
+	}
+	vs.push_back(v1);
+	vs.push_back(v2);
+	vs.push_back(v3);
+
+	vector<vector<int>>::iterator begin = vs.begin();
+	vector<vector<int>>::iterator end = vs.end();
+
+	while (begin != end) {
+		vector<int>::iterator sbegin = (*begin).begin();
+		vector<int>::iterator send = (*begin).end();
+
+		while (sbegin != send) {
+			cout << *sbegin << " ";
+			++sbegin;
+		}
+		++begin;
+		cout << endl;
+	}
+
+}
+void stringFunc01() {
+	// string 构造函数
+	string s1;
+	string s2(10, 'a');
+	string s3(s2);
+	string s4("hello");
+
+	// 赋值操作
+
+
+
+}
+int main() {
+	stringFunc01();
+	test05();
+	//test04();
+	//test03();
+	//test02();
+	//test01();
+	return 0;
+}
